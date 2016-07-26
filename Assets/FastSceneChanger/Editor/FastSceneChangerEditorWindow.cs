2a42cad2eb2a56345ee55cd229c8c3b50ba8ad25
HisaCat <ahisacat@gmail.com>
2016-07-26 22:28:42 +0900

    make FastSceneChangerEditorWindow class

---
+++ Assets/FastSceneChanger/Editor/FastSceneChangerEditorWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+/*
+* Copyright 2016, HisaCat https://github.com/hisacat
+* Released under the MIT license
+*/
+
+/// <summary>
+/// This is the class for fast switching of scene,
+/// for development convenience.
+/// </summary>
+public class FastSceneChangerEditorWindow : EditorWindow
+{
+
+    [MenuItem("Window/Fast Scene Changer")]
+    static void OpenWindow()
+    {
+        FastSceneChangerEditorWindow window = (FastSceneChangerEditorWindow)EditorWindow.GetWindow(typeof(FastSceneChangerEditorWindow));
+        Texture icon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Sprites/Gear.png");
+        GUIContent titleContent = new GUIContent("Scene", icon);
+        window.titleContent = titleContent;
+
+        window.Show();
+    }
+}
