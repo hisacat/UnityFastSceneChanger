c318d35acfdc94636f572f2a88248dcb847a7ed2
HisaCat <ahisacat@gmail.com>
2016-07-27 20:13:22 +0900

    show all scene dev

--- Assets/FastSceneChanger/Editor/FastSceneChangerEditorWindow.cs
+++ Assets/FastSceneChanger/Editor/FastSceneChangerEditorWindow.cs
@@ -30,7 +30,11 @@ public class FastSceneChangerEditorWindow : EditorWindow
 
     void OnDisable()
     {
+        selectedSceneIndex = -1;
 
+        viewAllScene = false;
+        allScenes = null;
+        GC.Collect();
     }
 
     void OnEnable()
@@ -41,18 +45,26 @@ public class FastSceneChangerEditorWindow : EditorWindow
 
     void OnLostFocus()
     {
-        //lastClickScene = -1;
+        selectedSceneIndex = -1;
     }
 
     void InitEyeIcon()
     {
         eyeIcon = EditorGUIUtility.FindTexture("ViewToolOrbit");
     }
+
+    int buildSceneCount = -1;
+
+    int selectedSceneIndex = -1;                        //Last click scene's index (if Last click in "All Scenes", plus Scenes In Build's Count)
+
     #region For double click check
-    int lastClickScene = -1;                        //Last click scene's index (if Last click in "All Scenes", plus Scenes In Build's Count)
+    int lastClickedScene = -1;                     //Last click scene's index (if Last click in "All Scenes", plus Scenes In Build's Count) for double click
     float lastClickTime = 0;
     const float DoubleClickDelay = 0.25f;
     #endregion
+
+    bool viewAllScene = false;
+    EditorBuildSettingsScene[] allScenes = null;
 
     #region GUI Styles
     bool isGuiStyleInitedWhenProSkin = false;
@@ -70,6 +82,8 @@ public class FastSceneChangerEditorWindow : EditorWindow
 
     Color proSelectBackground = Color.clear;
     Color proDisableSelectBackground = Color.clear;
+    Color proLostFocusSelectBackground = Color.clear;
+    Color proLostFocusDisableSelectBackground = Color.clear;
 
     Color stdTextColor = Color.clear;
     Color stdSelectTextColor = Color.clear;
@@ -78,6 +92,8 @@ public class FastSceneChangerEditorWindow : EditorWindow
 
     Color stdSelectBackground = Color.clear;
     Color stdDisableSelectBackground = Color.clear;
+    Color stdLostFocusSelectBackground = Color.clear;
+    Color stdLostFocusDisableSelectBackground = Color.clear;
     #endregion
 
     #region for view type icon
@@ -108,7 +124,6 @@ public class FastSceneChangerEditorWindow : EditorWindow
         if (viewOptionPopupStyle == null || isGuiStyleInitedWhenProSkin != EditorGUIUtility.isProSkin)
         {
             viewOptionPopupStyle = new GUIStyle((GUIStyle)"TE Toolbar");
-
             GUIStyleState style = new GUIStyleState();
             style.background = eyeIcon;
             style.textColor = Color.clear;
@@ -118,22 +133,23 @@ public class FastSceneChangerEditorWindow : EditorWindow
         if (sceneBoxStyle == null || isGuiStyleInitedWhenProSkin != EditorGUIUtility.isProSkin)
         {
             sceneBoxStyle = new GUIStyle((GUIStyle)"OL box");
-
             sceneBoxStyle.margin = new RectOffset(10, 10, 4, 4);
+            sceneBoxStyle.border = new RectOffset(2, 2, 2, 2);
             sceneBoxStyle.stretchHeight = false;
+            sceneBoxStyle.stretchWidth = true;
         }
         if (sceneTextStyle == null || isGuiStyleInitedWhenProSkin != EditorGUIUtility.isProSkin)
         {
             sceneTextStyle = new GUIStyle(EditorStyles.label);
             sceneTextStyle.padding = new RectOffset(10, 10, 2, 2);
-            sceneTextStyle.margin = new RectOffset(1, 1, 0, 0);
+            sceneTextStyle.margin = new RectOffset(1, 0, 0, 0);
             sceneTextStyle.active = sceneTextStyle.onActive = sceneTextStyle.normal = sceneTextStyle.onNormal;
         }
         if (sceneSelectedTextStyle == null || isGuiStyleInitedWhenProSkin != EditorGUIUtility.isProSkin)
         {
             sceneSelectedTextStyle = new GUIStyle(EditorStyles.label);
             sceneSelectedTextStyle.padding = new RectOffset(10, 10, 2, 2);
-            sceneSelectedTextStyle.margin = new RectOffset(1, 1, 0, 0);
+            sceneSelectedTextStyle.margin = new RectOffset(1, 0, 0, 0);
             sceneSelectedTextStyle.onNormal.textColor = Color.white;
             sceneSelectedTextStyle.active = sceneSelectedTextStyle.onActive = sceneSelectedTextStyle.normal = sceneSelectedTextStyle.onNormal;
         }
@@ -146,8 +162,10 @@ public class FastSceneChangerEditorWindow : EditorWindow
         proDisableTextColor = (Color)new Color32(255, 255, 255, 120);
 
         proSelectBackground = (Color)new Color32(62, 95, 150, 255);
-        //proDisableSelectBackground = (Color)new Color32(62, 87, 129, 255);
         proDisableSelectBackground = (Color)new Color32(62, 95, 150, 200);
+
+        proLostFocusSelectBackground = (Color)new Color32(255, 255, 255, 20);
+        proLostFocusDisableSelectBackground = (Color)new Color32(255, 255, 255, 10);
 
         stdTextColor = (Color)new Color32(0, 0, 0, 255);
         stdSelectTextColor = (Color)new Color32(255, 255, 255, 255);
@@ -155,8 +173,10 @@ public class FastSceneChangerEditorWindow : EditorWindow
         stdSelectDisableTextColor = (Color)new Color32(255, 255, 255, 120);
 
         stdSelectBackground = (Color)new Color32(62, 125, 231, 255);
-        //stdDisableSelectBackground = (Color)new Color32(102, 149, 229, 255);
         stdDisableSelectBackground = (Color)new Color32(62, 125, 231, 200);
+
+        stdLostFocusSelectBackground = (Color)new Color32(0, 0, 0, 80);
+        stdLostFocusDisableSelectBackground = (Color)new Color32(0, 0, 0, 50);
     }
     private int nowSceneIndex = 0;
 
@@ -179,6 +199,8 @@ public class FastSceneChangerEditorWindow : EditorWindow
         GUILayout.EndVertical();
 
 
+        bool sceneClicked = false;
+
         scrollPosition = GUILayout.BeginScrollView(scrollPosition, (GUIStyle)"hostview");
         {
             nowSceneIndex = 0;
@@ -198,34 +220,70 @@ public class FastSceneChangerEditorWindow : EditorWindow
                 {
                     bool isActiveScene = (activeScene.path == scenes[i].path);
 
-                    DrawSceneButton(scenes[i], isActiveScene, defColor);
+                    bool nowClicked = false;
+                    DrawSceneButton(scenes[i], isActiveScene, defColor, out nowClicked);
+                    sceneClicked = sceneClicked || nowClicked;
                 }
-
                 #endregion
                 GUILayout.Space(50);
 
             }
             GUILayout.EndVertical();
 
-            GUILayout.BeginVertical(sceneBoxStyle);
+            if (viewAllScene)
             {
-                GUILayout.Label("All Scenes", (GUIStyle)"OL Title");
-                #region Show All Scenes
+                GUILayout.BeginVertical(sceneBoxStyle);
+                {
+                    GUILayout.Label("All Scenes", (GUIStyle)"OL Title");
+                    #region Show All Scenes
 
-                #endregion
-                GUILayout.Space(50);
+                    //Get build scenes
+                    int scenesCount = allScenes == null ? 0 : allScenes.Length;
 
+                    for (int i = 0; i < scenesCount; i++)
+                    {
+                        bool isActiveScene = (activeScene.path == allScenes[i].path);
+
+                        bool nowClicked = false;
+                        DrawSceneButton(allScenes[i], isActiveScene, defColor, out nowClicked);
+                        sceneClicked = sceneClicked || nowClicked;
+                    }
+                    #endregion
+                    GUILayout.Space(50);
+                }
+                GUILayout.EndVertical();
             }
-            GUILayout.EndVertical();
+            else
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Space(sceneBoxStyle.margin.left);
+                    if (GUILayout.Button("View All Scenes"))
+                    {
+                        viewAllScene = true;
+                        allScenes = LoadAllScenes();
+                    }
+                    GUILayout.Space(sceneBoxStyle.margin.right);
+                }
+                GUILayout.EndHorizontal();
+            }
         }
         GUILayout.EndScrollView();
+
+        if (Event.current.type == EventType.mouseDown && !sceneClicked)
+        {
+            selectedSceneIndex = -1;
+        }
+
 
         GUI.color = defColor;
         Repaint();
     }
 
-    void DrawSceneButton(EditorBuildSettingsScene scene, bool isActiveScene, Color defColor)
+    void DrawSceneButton(EditorBuildSettingsScene scene, bool isActiveScene, Color defColor, out bool nowClicked)
     {
+        nowClicked = false;
+
         #region get sceneName(path)
         string sceneName = "";
         switch (viewOption)
@@ -239,7 +297,8 @@ public class FastSceneChangerEditorWindow : EditorWindow
         }
         #endregion
 
-        bool isNowSelectedScene = (lastClickScene == nowSceneIndex);
+        #region draw scene text
+        bool isNowSelectedScene = (selectedSceneIndex == nowSceneIndex);
 
         GUIContent sceneText = new GUIContent(sceneName);
         Rect sceneTextRect = GUILayoutUtility.GetRect(sceneText, sceneTextStyle);
@@ -247,8 +306,12 @@ public class FastSceneChangerEditorWindow : EditorWindow
         //Draw background when selected
         if (isNowSelectedScene)
         {
-            GUI.color = EditorGUIUtility.isProSkin ? (scene.enabled ? proSelectBackground : proDisableSelectBackground) :
+            if (focusedWindow == this)
+                GUI.color = EditorGUIUtility.isProSkin ? (scene.enabled ? proSelectBackground : proDisableSelectBackground) :
                                                      (scene.enabled ? stdSelectBackground : stdDisableSelectBackground);
+            else
+                GUI.color = EditorGUIUtility.isProSkin ? (scene.enabled ? proLostFocusSelectBackground : proLostFocusDisableSelectBackground) :
+                                                     (scene.enabled ? stdLostFocusSelectBackground : stdLostFocusDisableSelectBackground);
             GUI.DrawTexture(sceneTextRect, EditorGUIUtility.whiteTexture);
         }
 
@@ -259,19 +322,28 @@ public class FastSceneChangerEditorWindow : EditorWindow
         nowSceneTextStyle.fontStyle = isActiveScene ? FontStyle.Bold : FontStyle.Normal;
 
         GUI.Label(sceneTextRect, sceneText, nowSceneTextStyle);
+        #endregion
 
+        #region when click
         Event e = Event.current;
         if (sceneTextRect.Contains(e.mousePosition))
         {
             if (e.type == EventType.mouseDown)
             {
-                lastClickScene = nowSceneIndex;
+                nowClicked = true;
+                selectedSceneIndex = nowSceneIndex;
             }
             else if (e.type == EventType.mouseUp)
             {
                 if (e.button == 0)
                 {
-
+                    if (lastClickedScene == nowSceneIndex && Time.realtimeSinceStartup - lastClickTime <= DoubleClickDelay)
+                        SceneMenuCallback(new object[] { scene, SceneMenuType.Open });
+                    else
+                    {
+                        lastClickedScene = nowSceneIndex;
+                        lastClickTime = Time.realtimeSinceStartup;
+                    }
                 }
                 else if (e.button == 1)
                 {
@@ -285,7 +357,7 @@ public class FastSceneChangerEditorWindow : EditorWindow
                 }
             }
         }
-
+        #endregion
         nowSceneIndex++;
         GUI.color = defColor;
     }
@@ -313,30 +385,47 @@ public class FastSceneChangerEditorWindow : EditorWindow
         switch (type)
         {
             case SceneMenuType.ShowInProject:
+                EditorUtility.FocusProjectWindow();
                 Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(scene.path);
                 break;
             case SceneMenuType.ShowInExplorer:
                 EditorUtility.OpenWithDefaultApp(System.IO.Path.GetDirectoryName(scene.path));
                 break;
             case SceneMenuType.Open:
-                EditorSceneManager.OpenScene(scene.path);
+                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                    EditorSceneManager.OpenScene(scene.path);
                 break;
             case SceneMenuType.ShowInAddtive:
                 EditorSceneManager.OpenScene(scene.path, OpenSceneMode.Additive);
                 break;
         }
     }
-    void GetAllScenes()
+
+    EditorBuildSettingsScene[] LoadAllScenes()
     {
         string folderName = Application.dataPath;
         System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(folderName);
         System.IO.FileInfo[] allFileInfos = dirInfo.GetFiles("*.unity", System.IO.SearchOption.AllDirectories);
 
         int count = allFileInfos.Length;
+        EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[count];
+
         int removeCount = Application.dataPath.Length - 6;
         for (int i = 0; i < count; i++)
         {
-            Debug.Log(allFileInfos[i].FullName.Remove(0, removeCount));
+            scenes[i] = new EditorBuildSettingsScene(allFileInfos[i].FullName.Remove(0, removeCount), true);
         }
+        return scenes;
+    }
+
+    public void SceneRemoved(string path)
+    {
+        if (!viewAllScene || allScenes == null)
+            return;
+    }
+    public void SceneAdded(string path)
+    {
+        if (!viewAllScene || allScenes == null)
+            return;
     }
 }
