6f81d24aa3f9a211974dcfbd226841d5da652024
HisaCat <ahisacat@gmail.com>
2016-07-27 17:39:28 +0900

    Open scene dev change some GUI Colors Scene button to text, check mouse event with eventsystem

--- Assets/FastSceneChanger/Editor/FastSceneChangerEditorWindow.cs
+++ Assets/FastSceneChanger/Editor/FastSceneChangerEditorWindow.cs
@@ -39,6 +39,11 @@ public class FastSceneChangerEditorWindow : EditorWindow
         InitEyeIcon();
     }
 
+    void OnLostFocus()
+    {
+        //lastClickScene = -1;
+    }
+
     void InitEyeIcon()
     {
         eyeIcon = EditorGUIUtility.FindTexture("ViewToolOrbit");
@@ -56,6 +61,7 @@ public class FastSceneChangerEditorWindow : EditorWindow
     GUIStyle sceneBoxStyle = null;
 
     GUIStyle sceneTextStyle = null;
+    GUIStyle sceneSelectedTextStyle = null;
     #endregion
 
     #region Colors
@@ -63,10 +69,12 @@ public class FastSceneChangerEditorWindow : EditorWindow
     Color proDisableTextColor = Color.clear;
 
     Color proSelectBackground = Color.clear;
-    Color proDisalbeSelectBackground = Color.clear;
+    Color proDisableSelectBackground = Color.clear;
 
     Color stdTextColor = Color.clear;
+    Color stdSelectTextColor = Color.clear;
     Color stdDisableTextColor = Color.clear;
+    Color stdSelectDisableTextColor = Color.clear;
 
     Color stdSelectBackground = Color.clear;
     Color stdDisableSelectBackground = Color.clear;
@@ -117,7 +125,13 @@ public class FastSceneChangerEditorWindow : EditorWindow
         if (sceneTextStyle == null || isGuiStyleInitedWhenProSkin != EditorGUIUtility.isProSkin)
         {
             sceneTextStyle = new GUIStyle(EditorStyles.label);
-            sceneTextStyle.active = sceneTextStyle.onActive = sceneTextStyle.normal;
+            sceneTextStyle.active = sceneTextStyle.onActive = sceneTextStyle.normal = sceneTextStyle.onNormal;
+        }
+        if (sceneSelectedTextStyle == null || isGuiStyleInitedWhenProSkin != EditorGUIUtility.isProSkin)
+        {
+            sceneSelectedTextStyle = new GUIStyle(EditorStyles.label);
+            sceneSelectedTextStyle.onNormal.textColor = Color.white;
+            sceneSelectedTextStyle.active = sceneSelectedTextStyle.onActive = sceneSelectedTextStyle.normal = sceneSelectedTextStyle.onNormal;
         }
 
         isGuiStyleInitedWhenProSkin = EditorGUIUtility.isProSkin;
@@ -128,21 +142,26 @@ public class FastSceneChangerEditorWindow : EditorWindow
         proDisableTextColor = (Color)new Color32(255, 255, 255, 120);
 
         proSelectBackground = (Color)new Color32(62, 95, 150, 255);
-        proDisalbeSelectBackground = (Color)new Color32(62, 87, 129, 255);
+        //proDisableSelectBackground = (Color)new Color32(62, 87, 129, 255);
+        proDisableSelectBackground = (Color)new Color32(62, 95, 150, 200);
 
         stdTextColor = (Color)new Color32(0, 0, 0, 255);
+        stdSelectTextColor = (Color)new Color32(255, 255, 255, 255);
         stdDisableTextColor = (Color)new Color32(0, 0, 0, 120);
+        stdSelectDisableTextColor = (Color)new Color32(255, 255, 255, 120);
 
         stdSelectBackground = (Color)new Color32(62, 125, 231, 255);
-        stdDisableSelectBackground = (Color)new Color32(102, 149, 229, 255);
+        //stdDisableSelectBackground = (Color)new Color32(102, 149, 229, 255);
+        stdDisableSelectBackground = (Color)new Color32(62, 125, 231, 200);
     }
+    private int nowSceneIndex = 0;
+
     void OnGUI()
     {
-        InitStyles();
         InitColors();
+        InitStyles();
 
         Color defColor = GUI.color;
-
 
         GUILayout.BeginVertical(topMenuStyle);
         {
@@ -158,6 +177,7 @@ public class FastSceneChangerEditorWindow : EditorWindow
 
         scrollPosition = GUILayout.BeginScrollView(scrollPosition, (GUIStyle)"hostview");
         {
+            nowSceneIndex = 0;
             //get active scenes
             Scene activeScene = EditorSceneManager.GetActiveScene();
 
@@ -172,35 +192,9 @@ public class FastSceneChangerEditorWindow : EditorWindow
 
                 for (int i = 0; i < scenesCount; i++)
                 {
-                    bool isEnabled = scenes[i].enabled;
                     bool isActiveScene = (activeScene.path == scenes[i].path);
 
-                    #region get sceneName(path)
-                    string sceneName = "";
-                    switch (viewOption)
-                    {
-                        case 0: //Show full path (remove "ASSETS/")
-                            sceneName = scenes[i].path.Remove(0, 7);
-                            break;
-                        case 1: //Show only scene name
-                            sceneName = System.IO.Path.GetFileName(scenes[i].path);
-                            break;
-                    }
-                    #endregion
-
-                    sceneName = isActiveScene ? "▶" + sceneName : "　" + sceneName;
-
-                    GUIContent buttonText = new GUIContent(sceneName);
-                    Rect rect = GUILayoutUtility.GetRect(buttonText, sceneTextStyle);
-
-                    GUI.color = isEnabled ? (EditorGUIUtility.isProSkin ? proTextColor : stdTextColor) :
-                                            (EditorGUIUtility.isProSkin ? proDisableTextColor : stdDisableTextColor);
-
-                    if (GUI.Button(rect, buttonText, sceneTextStyle) && isEnabled)
-                    {
-                        Debug.Log("ASE");
-                    }
-                    GUI.color = defColor;
+                    DrawSceneButton(scenes[i], isActiveScene, defColor);
                 }
 
                 #endregion
@@ -226,6 +220,109 @@ public class FastSceneChangerEditorWindow : EditorWindow
         Repaint();
     }
 
+    void DrawSceneButton(EditorBuildSettingsScene scene, bool isActiveScene, Color defColor)
+    {
+        #region get sceneName(path)
+        string sceneName = "";
+        switch (viewOption)
+        {
+            case 0: //Show full path (remove "ASSETS/")
+                sceneName = scene.path.Remove(0, 7);
+                break;
+            case 1: //Show only scene name
+                sceneName = System.IO.Path.GetFileName(scene.path);
+                break;
+        }
+        #endregion
+
+        bool isNowSelectedScene = (lastClickScene == nowSceneIndex);
+        sceneName = isActiveScene ? "▶" + sceneName : "　" + sceneName;
+
+        GUIContent buttonText = new GUIContent(sceneName);
+        Rect sceneTextRect = GUILayoutUtility.GetRect(buttonText, sceneTextStyle);
+
+        //Draw background when selected
+        if (isNowSelectedScene)
+        {
+            GUI.color = EditorGUIUtility.isProSkin ? (scene.enabled ? proSelectBackground : proDisableSelectBackground) :
+                                                     (scene.enabled ? stdSelectBackground : stdDisableSelectBackground);
+            GUI.DrawTexture(sceneTextRect, EditorGUIUtility.whiteTexture);
+        }
+
+        GUI.color = scene.enabled ? (EditorGUIUtility.isProSkin ? proTextColor : (isNowSelectedScene ? stdSelectTextColor : stdTextColor)) :
+                                    (EditorGUIUtility.isProSkin ? proDisableTextColor : (isNowSelectedScene ? stdSelectDisableTextColor : stdDisableTextColor));
+
+        if (isNowSelectedScene)
+            GUI.Label(sceneTextRect, buttonText, sceneSelectedTextStyle);
+        else
+            GUI.Label(sceneTextRect, buttonText, sceneTextStyle);
+
+        Event e = Event.current;
+        if (sceneTextRect.Contains(e.mousePosition))
+        {
+            if (e.type == EventType.mouseDown)
+            {
+                lastClickScene = nowSceneIndex;
+            }
+            else if (e.type == EventType.mouseUp)
+            {
+                if (e.button == 0)
+                {
+
+                }
+                else if (e.button == 1)
+                {
+                    GenericMenu menu = new GenericMenu();
+                    menu.AddItem(new GUIContent("Show in Project"), false, SceneMenuCallback, new object[] { scene, SceneMenuType.ShowInProject });
+                    menu.AddItem(new GUIContent("Show in Explorer"), false, SceneMenuCallback, new object[] { scene, SceneMenuType.ShowInExplorer });
+                    menu.AddSeparator("");
+                    menu.AddItem(new GUIContent("Open"), false, SceneMenuCallback, new object[] { scene, SceneMenuType.Open });
+                    menu.AddItem(new GUIContent("Open Scene Addtive"), false, SceneMenuCallback, new object[] { scene, SceneMenuType.ShowInAddtive });
+                    menu.ShowAsContext();
+                }
+            }
+        }
+
+        nowSceneIndex++;
+        GUI.color = defColor;
+    }
+
+    enum SceneMenuType : int
+    {
+        ShowInProject = 0,
+        ShowInExplorer = 1,
+        Open = 2,
+        ShowInAddtive = 3,
+    }
+    void SceneMenuCallback(object userData)
+    {
+        EditorBuildSettingsScene scene = (EditorBuildSettingsScene)((object[])userData)[0];
+        SceneMenuType type = (SceneMenuType)((object[])userData)[1];
+
+        string sceneFullPath = string.Concat(Application.dataPath, "/", scene.path.Remove(0, 7));
+        bool isSceneExits = System.IO.File.Exists(sceneFullPath);
+        if (!isSceneExits)
+        {
+            EditorUtility.DisplayDialog("Scene not found", string.Concat("Scene ", scene.path, " is not exists."), "Ok");
+            return;
+        }
+
+        switch (type)
+        {
+            case SceneMenuType.ShowInProject:
+                Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(scene.path);
+                break;
+            case SceneMenuType.ShowInExplorer:
+                EditorUtility.OpenWithDefaultApp(System.IO.Path.GetDirectoryName(scene.path));
+                break;
+            case SceneMenuType.Open:
+                EditorSceneManager.OpenScene(scene.path);
+                break;
+            case SceneMenuType.ShowInAddtive:
+                EditorSceneManager.OpenScene(scene.path, OpenSceneMode.Additive);
+                break;
+        }
+    }
     void GetAllScenes()
     {
         string folderName = Application.dataPath;
